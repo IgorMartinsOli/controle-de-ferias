9e1549e355087d21a0da0299369e382395286ae4
Igor_ <igrmartoli@gmail.com>
2022-07-27 20:29:58 -0300

    erro de relacionamento

--- solicitacoes/solicitacaoController.js
+++ solicitacoes/solicitacaoController.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const Solicitacao = require('./solicitacao');
 const Aquisitivo = require('../aquisitivo/Aquisitivo');
 
+
 router.get('/admin/solicitacoes', (req, res) => {
-    Solicitacao.findAll(
-        {include:[{model:Aquisitivo, required: true}], where: {status: false}}
+    Solicitacao.findAll({
+        include:[{model:Aquisitivo, required: true}], where: {status: false}},
+        {subQuery: false}
     ).then(solicitacoes => {
         res.render('admin/solicitacoes/index', {solicitacoes: solicitacoes});
     })
